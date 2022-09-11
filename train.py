7d77590c35e028e051b7cb8c3f03cd9762d5a2db
TheDryus <and.zayler2012@yandex.ru>
2022-09-11 13:44:02 +0300

    mf

--- train.py
+++ train.py
@@ -1,61 +1,56 @@
 # Файл для обучения
 
-def tokenizer():
-    import re  # библиотека регулярных выражений
-
-    answer = 1
-    text = ''
-    while answer != 2:
-        try:
-            answer = int(input("У вас есть данные, которые вы хотите считать? (1 - Да, 2 - Нет): "))
-
-            if answer == 1:
-                input_dir = input("Здравствуйте, укажите директорию, в которых хранятся файлы."
-                                  "\n(Пример директории: C:\\Users\\myPC\\Desktop\\My_text):")
-                try:
-                    # Считываем текст с файла в директории
-                    f = open(input_dir, "r", encoding="utf-8")
-                    file = f.read()
-                    f.close()
-
-                except FileNotFoundError:  # Пропускаем ошибку о том, что файл не найден
-                    print(f"Файла \"{input_dir}\" нет.")
-                    continue
-                text += file
-
-            if (answer != 1) and (answer != 2):
-                print("Введите \"1\" либо \"2\": ")
-                continue
+from gensim.models import Word2Vec as w2v
 
 
-        except ValueError:
-            print("Введите цифру!")
-
-    tokenizer = re. \
-        sub("[^А-яа-я]", " ", text) \
-        .lower() \
-        .split(" ")
-
-    for whitespace in tokenizer:
-        if len(whitespace) == 0:
-            tokenizer.remove(whitespace)
-
-    tokenizer.pop()
-    return tokenizer
-
-
-tokenizer = tokenizer()
-
-print(tokenizer)
-f = open("tokenizer.txt", "w", encoding="utf-8")
-f.write(str(tokenizer))
-f.close()
-# model = w2v(
-#     tokenizer.tokenizer,
-#     min_count=5,
-#     epochs=100,
-#     vector_size=100
-# )
+# def tokenizer():
+#     import re  # библиотека регулярных выражений
 #
-# model = model.wv.most_similar(["принц"], topn=5)
-# print(model)
+#     answer = 1
+#     text = ''
+#
+#     while answer != 2:
+#         try:
+#             answer = int(input("У вас есть данные, которые вы хотите считать? (1 - Да, 2 - Нет): "))
+#
+#             if answer == 1:
+#                 input_dir = input("Укажите директорию, в которых хранятся файлы."
+#                                   "\n(Пример директории: C:\\Users\\myPC\\Desktop\\My_text):")
+#                 try:
+#                     # Считываем текст с файла в директории
+#                     f = open(input_dir, "r", encoding="utf-8")
+#                     file = f.read()
+#                     f.close()
+#
+#                 except FileNotFoundError:  # Пропускаем ошибку о том, что файл не найден
+#                     print(f"Файла \"{input_dir}\" нет.")
+#                     continue
+#                 text += file
+#
+#             if (answer != 1) and (answer != 2):
+#                 print("Введите \"1\" либо \"2\": ")
+#                 continue
+#
+#
+#         except ValueError:
+#             print("Введите цифру!")
+#
+#     tokenizer = re \
+#         .sub("[^А-яа-я]", " ", text) \
+#         .lower() \
+#         .split()
+#
+#     # Записываем результат токенизации в файл
+#     f = open("tokenizer.txt", "w", encoding="utf-8")
+#     f.write(str(tokenizer))
+#     f.close()
+#
+#     return tokenizer
+#
+#
+# tokenizer = tokenizer()
+
+f = open("tokenizer.txt", "r", encoding="utf-8")
+tokenizer = f.read()
+f.close()
+
